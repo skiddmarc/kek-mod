7b183c7b9a2fa1f52f3659f5763f69177a31dd4e
skiddmarc <Tanner@DESKTOP-V7EGHFK>
2021-05-13 11:57:24 -0400

    Update 1

---
+++ src/main/java/net/mcreator/mymod/item/DioFaceFoodItem.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.mymod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+
+import net.mcreator.mymod.MymodModElements;
+
+@MymodModElements.ModElement.Tag
+public class DioFaceFoodItem extends MymodModElements.ModElement {
+	@ObjectHolder("mymod:dio_face_food")
+	public static final Item block = null;
+	public DioFaceFoodItem(MymodModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(1).rarity(Rarity.RARE)
+					.food((new Food.Builder()).hunger(4).saturation(0.4f).setAlwaysEdible().build()));
+			setRegistryName("dio_face_food");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.DRINK;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getEatSound() {
+			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(DioFaceFoodItem.block, (int) (1));
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/mymod/procedures/MegaGokuItemIsCraftedsmeltedProcedure.java
@@ -0,0 +1,44 @@
+package net.mcreator.mymod.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.mymod.MymodModElements;
+import net.mcreator.mymod.MymodMod;
+
+import java.util.Map;
+
+@MymodModElements.ModElement.Tag
+public class MegaGokuItemIsCraftedsmeltedProcedure extends MymodModElements.ModElement {
+	public MegaGokuItemIsCraftedsmeltedProcedure(MymodModElements instance) {
+		super(instance, 3);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MymodMod.LOGGER.warn("Failed to load dependency entity for procedure MegaGokuItemIsCraftedsmelted!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MymodMod.LOGGER.warn("Failed to load dependency world for procedure MegaGokuItemIsCraftedsmelted!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (world instanceof ServerWorld) {
+			LightningBoltEntity _ent = EntityType.LIGHTNING_BOLT.create((World) world);
+			_ent.moveForced(
+					Vector3d.copyCenteredHorizontally(new BlockPos((int) (entity.getPosX()), (int) (entity.getPosY()), (int) (entity.getPosZ()))));
+			_ent.setEffectOnly(true);
+			((World) world).addEntity(_ent);
+		}
+	}
+}
